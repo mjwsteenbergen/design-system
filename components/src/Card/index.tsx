b4bf1d4b03e9b51a175a18e68945faeef367608c
Martijn Steenbergen <m.uses+git@outlook.com>
2023-07-31 19:57:51 +0000

    cards do not have border

--- components/src/Card/index.tsx
+++ components/src/Card/index.tsx
@@ -2,36 +2,47 @@ import { cva } from "class-variance-authority";
 import { PropsWithChildren, ReactElement, ReactNode } from "react";
 
 type Props = {
-    header?: ReactNode,
-    content: ReactElement,
-    contained?: boolean;
-}
+  header?: ReactNode;
+  content: ReactElement;
+  contained?: boolean;
+};
 
 const variants = cva("rounded overflow-hidden", {
-    variants: {
-        contained: {
-            true: ["shadow border-2 border-gray-50 rounded-lg"]
-        }
-    }
-    
-})
+  variants: {
+    contained: {
+      true: ["shadow-lg rounded-lg"],
+    },
+  },
+});
 
 const childVariant = cva("py-2", {
-    variants: {
-        contained: {
-            true: ["px-4"]
-        }
-    }
-})
+  variants: {
+    contained: {
+      true: ["px-4"],
+    },
+  },
+});
 
-export const Card = ({ contained, children, header, content, className, ...rest }: PropsWithChildren<Props & React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>>) => {
-    
-    return <div {...rest} className={variants({ contained,className })}>
-        {header}
+export const Card = ({
+  contained,
+  children,
+  header,
+  content,
+  className,
+  ...rest
+}: PropsWithChildren<
+  Props &
+    React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLDivElement>,
+      HTMLDivElement
+    >
+>) => {
+  return (
+    <div {...rest} className={variants({ contained, className })}>
+      {header}
 
-        {content}
-        <div className={childVariant({contained})}>
-            {children}
-        </div>
+      {content}
+      <div className={childVariant({ contained })}>{children}</div>
     </div>
-}
+  );
+};
